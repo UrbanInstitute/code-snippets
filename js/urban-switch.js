cc18ee92a35ec55fdc965ebaa7e5b708c006e0f6
hrecht <hrecht@users.noreply.github.com>
2015-08-27 16:21:14 -0400

    add d3 functionality to switch

---
+++ js/urban-switch.js
@@ -0,0 +1,18 @@
+//for usage with d3 graphics: on clicking the switch, something happens in the graphics (example here - change the text's color)
+var show1 = 1;
+
+d3.select("#s1").on("click", function () {
+    if (show1 == 1) {
+        d3.select("#s1.switch")
+            .attr("class", "switch off");
+        d3.select("#onoff")
+            .style("color", "#1696d2");
+        show1 = 0;
+    } else {
+        d3.select("#s1.switch")
+            .attr("class", "switch on");
+        d3.select("#onoff")
+            .style("color", "#666");
+        show1 = 1;
+    }
+});
